bc71b93acc30613ded51b988828e1f4c216a9452
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-06-05 14:40:18 +0530

    redux/toolkit is installed and store is created.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { configureStore } from '@reduxjs/toolkit';
+import movie from './reducers';
+
+
+const store = configureStore({ reducer:movie});
+console.log(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
