8aff1b2af1e72983360fed9994b1428cf098e79d
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-06-05 15:29:40 +0530

    subscribe and dispatch methods are used for rendering the data.

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import movie from './reducers';
 
 
 const store = configureStore({ reducer:movie});
-console.log(store.getState());
-store.dispatch({type: 'ADD_MOVIES', movies: [{name: 'Superman'}]});
-console.log(store.getState());
+// console.log(store);
+// store.dispatch({type: 'ADD_MOVIES', movies: [{name: 'Superman'}]});
+// console.log(store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>
 );
